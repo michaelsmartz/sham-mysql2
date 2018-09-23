23dbcd7bc86559d98b771bbbeedff58e54685166
jasonpolydor <jasonpolydor@gmail.com>
2018-09-23 07:55:56 +0400

    correction e-learning

--- resources/views/training_delivery_methods/form.blade.php
+++ resources/views/training_delivery_methods/form.blade.php
@@ -2,7 +2,7 @@
     
 <div class="form-group col-xs-12 {{ $errors->has('description') ? 'has-error' : '' }}">
     <label for="description">Description</label>
-        <input class="form-control" name="description" type="text" id="description" value="{{ old('description', optional($trainingDeliveryMethod)->description) }}" minlength="1" maxlength="100" placeholder="Enter description">
+        <input class="form-control" name="description" type="text" id="description" value="{{ old('description', optional($trainingDeliveryMethod)->description) }}" minlength="1" maxlength="100" required="true" placeholder="Enter description">
         {!! $errors->first('description', '<p class="help-block">:message</p>') !!}
 </div>
 
