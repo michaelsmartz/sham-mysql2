b2884b6496c5b4c9f576bd407aa97412bf84ff8e
jasonpolydor <jasonpolydor@gmail.com>
2018-09-24 16:24:50 +0400

    in progress user form

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,36 +1,25 @@
 @extends(Request::ajax()?'blank':'portal-index')
 @section('title', 'Add User')
-@section('modalTitle', 'Edit User')
-
-@section('modalFooter')
-    <input class="btn btn-primary pull-right" type="submit" value="Add">
-    <a href="{{ route('users.index') }}" class="btn btn-default pull-right" title="Show all Users">
-        <span class="glyphicon glyphicon-th-list" aria-hidden="true"></span>
-    </a>
-@endsection
-
-@section('modalContent')
-    <form method="POST" action="{{ route('users.store') }}" accept-charset="UTF-8" id="create_user_form" name="create_user_form" class="form-horizontal">
-        {{ csrf_field() }}
-        <div class="row">
-            <div class="col-sm-12">
-                @include('users.form', [
-                    'user' => null,
-                ])
-            </div>
-        </div>
-        @if(!Request::ajax())
-        <div class="box-footer">
-            @yield('modalFooter') 
-        </div>
-        @endif
-    </form>
-@endsection
 
 @section('content')
     <div class="box box-primary">
         <div class="box-body">
-                @yield('modalContent') 
+            <form method="POST" action="{{ route('users.store') }}" accept-charset="UTF-8" id="create_user_form" name="create_user_form" class="form-horizontal">
+                {{ csrf_field() }}
+                <div class="row">
+                    <div class="col-sm-12">
+                        @include('users.form', [
+                            'user' => null,
+                        ])
+                    </div>
+                </div>
+                <div class="box-footer">
+                    <input class="btn btn-primary pull-right" type="submit" value="Add">
+                    <a href="{{ route('users.index') }}" class="btn btn-default pull-right" title="Show all Users">
+                        <span class="glyphicon glyphicon-th-list" aria-hidden="true"></span>
+                    </a>
+                </div>
+            </form>
         </div>
     </div>
 @endsection
--- resources/views/users/form.blade.php
+++ resources/views/users/form.blade.php
@@ -1,67 +1,66 @@
 <div class="row">
-    
-<div class="form-group col-xs-12 {{ $errors->has('username') ? 'has-error' : '' }}">
-    <label for="username">Username</label>
-        <input class="form-control" name="username" type="text" id="username" value="{{ old('username', optional($user)->username) }}" minlength="1" required="true" placeholder="Enter username">
-        {!! $errors->first('username', '<p class="help-block">:message</p>') !!}
-</div>
+    <div class="form-group col-xs-6 {{ $errors->has('username') ? 'has-error' : '' }}">
+        <label for="username">Username</label>
+            <input class="form-control" name="username" type="text" id="username" value="{{ old('username', optional($user)->username) }}" minlength="1" required="true" placeholder="Enter username">
+            {!! $errors->first('username', '<p class="help-block">:message</p>') !!}
+    </div>
 
-<div class="form-group col-xs-12 {{ $errors->has('password') ? 'has-error' : '' }}">
-    <label for="password">Password</label>
-        <input class="form-control" name="password" type="password" id="password" value="{{ old('password', optional($user)->password) }}" placeholder="Enter password">
-        {!! $errors->first('password', '<p class="help-block">:message</p>') !!}
-</div>
+    <div class="form-group col-xs-6 {{ $errors->has('password') ? 'has-error' : '' }}">
+        <label for="password">Password</label>
+            <input class="form-control" name="password" type="password" id="password" value="{{ old('password', optional($user)->password) }}" placeholder="Enter password">
+            {!! $errors->first('password', '<p class="help-block">:message</p>') !!}
+    </div>
 
-<div class="form-group col-xs-12 {{ $errors->has('email') ? 'has-error' : '' }}">
-    <label for="email">Email</label>
-        <input class="form-control" name="email" type="email" id="email" value="{{ old('email', optional($user)->email) }}" placeholder="Enter email">
-        {!! $errors->first('email', '<p class="help-block">:message</p>') !!}
-</div>
+    <div class="form-group col-xs-6 {{ $errors->has('email') ? 'has-error' : '' }}">
+        <label for="email">Email</label>
+            <input class="form-control" name="email" type="email" id="email" value="{{ old('email', optional($user)->email) }}" placeholder="Enter email">
+            {!! $errors->first('email', '<p class="help-block">:message</p>') !!}
+    </div>
 
-<div class="form-group col-xs-12 {{ $errors->has('cell_number') ? 'has-error' : '' }}">
-    <label for="cell_number">Cell Number</label>
-        <input class="form-control" name="cell_number" type="number" id="cell_number" value="{{ old('cell_number', optional($user)->cell_number) }}" placeholder="Enter cell number">
-        {!! $errors->first('cell_number', '<p class="help-block">:message</p>') !!}
-</div>
+    <div class="form-group col-xs-6 {{ $errors->has('cell_number') ? 'has-error' : '' }}">
+        <label for="cell_number">Cell Number</label>
+            <input class="form-control" name="cell_number" type="number" id="cell_number" value="{{ old('cell_number', optional($user)->cell_number) }}" placeholder="Enter cell number">
+            {!! $errors->first('cell_number', '<p class="help-block">:message</p>') !!}
+    </div>
 
-<div class="form-group col-xs-12 {{ $errors->has('silence_start') ? 'has-error' : '' }}">
-    <label for="silence_start">Silence Start</label>
-        <input class="form-control timepicker" name="silence_start" type="text" id="silence_start" value="{{ old('silence_start', optional($user)->silence_start) }}" minlength="1" placeholder="Enter silence start">
-        {!! $errors->first('silence_start', '<p class="help-block">:message</p>') !!}
-</div>
-
-<div class="form-group col-xs-12 {{ $errors->has('silence_end') ? 'has-error' : '' }}">
-    <label for="silence_end">Silence End</label>
-        <input class="form-control timepicker" name="silence_end" type="text" id="silence_end" value="{{ old('silence_end', optional($user)->silence_end) }}" minlength="1" placeholder="Enter silence end">
-        {!! $errors->first('silence_end', '<p class="help-block">:message</p>') !!}
-</div>
-
-<div class="form-group col-xs-12 {{ $errors->has('sham_user_profile_id') ? 'has-error' : '' }}">
-    <label for="sham_user_profile_id">User Profile</label>
+    <div class="form-group col-xs-12 {{ $errors->has('sham_user_profile_id') ? 'has-error' : '' }}">
+        <label for="sham_user_profile_id">User Profile</label>
         <select class="form-control" id="sham_user_profile_id" name="sham_user_profile_id" required="true">
-        	    <option value="" style="display: none;" {{ old('sham_user_profile_id', optional($user)->sham_user_profile_id ?: '') == '' ? 'selected' : '' }} disabled selected>Select Sham User Profile</option>
-        	@foreach ($sham_user_profile_ids as $key => $sham_user_profile_id)
-			    <option value="{{ $key }}" {{ old('sham_user_profile_id', optional($user)->sham_user_profile_id) == $key ? 'selected' : '' }}>
-			    	{{ $sham_user_profile_id }}
-			    </option>
-			@endforeach
+            <option value="" style="display: none;" {{ old('sham_user_profile_id', optional($user)->sham_user_profile_id ?: '') == '' ? 'selected' : '' }} disabled selected>Select Sham User Profile</option>
+            @foreach ($sham_user_profile_ids as $key => $sham_user_profile_id)
+                <option value="{{ $key }}" {{ old('sham_user_profile_id', optional($user)->sham_user_profile_id) == $key ? 'selected' : '' }}>
+                    {{ $sham_user_profile_id }}
+                </option>
+            @endforeach
         </select>
-        
-        {!! $errors->first('sham_user_profile_id', '<p class="help-block">:message</p>') !!}
-</div>
 
-<div class="form-group col-xs-12 {{ $errors->has('employee_id') ? 'has-error' : '' }}">
-    <label for="employee_id">Employee</label>
+        {!! $errors->first('sham_user_profile_id', '<p class="help-block">:message</p>') !!}
+    </div>
+
+    <div class="form-group col-xs-12 {{ $errors->has('employee_id') ? 'has-error' : '' }}">
+        <label for="employee_id">Employee</label>
         <select class="form-control" id="employee_id" name="employee_id" required="true">
-        	    <option value="" style="display: none;" {{ old('employee_id', optional($user)->employee_id ?: '') == '' ? 'selected' : '' }} disabled selected>Select Employee</option>
-        	@foreach ($employee_ids as $key => $employee_id)
-			    <option value="{{ $key }}" {{ old('employee_id', optional($user)->employee_id) == $key ? 'selected' : '' }}>
-			    	{{ $employee_id }}
-			    </option>
-			@endforeach
+            <option value="" style="display: none;" {{ old('employee_id', optional($user)->employee_id ?: '') == '' ? 'selected' : '' }} disabled selected>Select Employee</option>
+            @foreach ($employee_ids as $key => $employee_id)
+                <option value="{{ $key }}" {{ old('employee_id', optional($user)->employee_id) == $key ? 'selected' : '' }}>
+                    {{ $employee_id }}
+                </option>
+            @endforeach
         </select>
-        
+
         {!! $errors->first('employee_id', '<p class="help-block">:message</p>') !!}
-</div>
+    </div>
+
+    <div class="form-group col-xs-6 {{ $errors->has('silence_start') ? 'has-error' : '' }}">
+        <label for="silence_start">Silence Start</label>
+            <input class="form-control timepicker" name="silence_start" type="text" id="silence_start" value="{{ old('silence_start', optional($user)->silence_start) }}" minlength="1" placeholder="Enter silence start">
+            {!! $errors->first('silence_start', '<p class="help-block">:message</p>') !!}
+    </div>
+
+    <div class="form-group col-xs-6 {{ $errors->has('silence_end') ? 'has-error' : '' }}">
+        <label for="silence_end">Silence End</label>
+            <input class="form-control timepicker" name="silence_end" type="text" id="silence_end" value="{{ old('silence_end', optional($user)->silence_end) }}" minlength="1" placeholder="Enter silence end">
+            {!! $errors->first('silence_end', '<p class="help-block">:message</p>') !!}
+    </div>
 
 </div>
