f5499dedd0388f1da589b066436aac5e1dc0bc7b
pritam <mahenduth@kalijaglobal.com>
2018-12-28 16:25:02 +0400

    Add Recruitment Hired Prototype page.

---
+++ resources/views/recruitments/hired.blade.php
@@ -0,0 +1,47 @@
+<ul class="people-list">
+    <li class="active">
+        <div class="img">
+        </div>
+        <div>
+            <strong>Robert Sons</strong>
+            <div>IT Manager</div>
+        </div>
+    </li>
+</ul>
+<div class="tab-detail">
+    <div class="row">
+        <div class="col-md-2 img">
+            <div class="avatar-upload">
+                <div class="avatar-edit">
+                    <input type='file' name="profile_pic" id="imageUpload" accept=".png, .jpg, .jpeg" />
+                    <label for="imageUpload" title="change profile image"></label>
+                </div>
+                <div class="avatar-preview">
+                    <div id="imagePreview" style="background-image: url('/img/avatar.png');">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div>
+                <h2>Robert Sons</h2>
+                <div>IT Manager</div>
+                <br>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <label for="candidateName">Start Date</label>
+                    <input id="candidateName" class='form-control' type="text" name="candidateName" >
+                </div>
+                <div class="col-md-12">
+                    <label for="comments">Comments</label>
+                    <textarea rows="4" cols="66"> </textarea>
+                </div>
+                <div class="col-md-12">
+                    <button type="button" class="btn btn-success pull-right">Save</button>
+                    <button type="button" class="btn btn-success">Import Candidate Data</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
