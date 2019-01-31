3881fb88b43f112522c85245181e5c736e28bc0f
jasonpolydor <jasonpolydor@gmail.com>
2019-01-31 18:57:25 +0400

    validation duplicate question category in assessment

--- resources/views/assessments/create.blade.php
+++ resources/views/assessments/create.blade.php
@@ -4,7 +4,7 @@
 @section('content')
     <div class="box box-primary">
         <div class="box-body">
-            <form method="POST" action="{{ route('assessments.store') }}" accept-charset="UTF-8" id="create_assessment_form" name="create_assessment_form" class="form-horizontal">
+            <form method="POST" action="{{ route('assessments.store') }}" accept-charset="UTF-8" id="assessment_form" name="create_assessment_form" class="form-horizontal">
                 {{ csrf_field() }}
                 <div class="row">
                     <div class="col-sm-12">
@@ -41,6 +41,62 @@
                     }
                 });
             });
+
+            $('#assessment_form').submit( function(e)
+            {
+                //e.preventDefault();
+                var retvalue = true;
+                $.ajaxSetup({ headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') } });
+                var data = $('#multiselect_to').val();
+                data = {'AssessmentCateoryIds':$('#multiselect_to').val()};
+
+                $.ajax({
+                    url: '{{route('assessment.duplicates')}}',
+                    type: 'POST',
+                    data: data,
+                    async:false,
+                    // dataType: 'JSON',
+                    success: function (data) {
+                        var obj = data['questions'];
+                        $('#alert_placeholder').empty();
+                        if(data['duplicates'] == true)
+                        {
+                            retvalue = false;
+                            $('#alert_placeholder').append('<br>');
+                            $('#alert_placeholder').append('<div class="alert alert-danger"><strong>Error:</strong> The selected categories contain duplicate questions. Check list below for details.</div>')
+                            $('#alert_placeholder').append('<table class="table"><thead><tr><th>Questions</th><th>Present in categories</th></tr></thead><tbody></tbody></table>');
+                            $.each(obj, function(){
+                                var categories = this['categories'];
+                                var categoriestxt = ''
+                                $.each(categories, function(){
+                                    categoriestxt = categoriestxt + this + ', ';
+                                });
+                                if(categoriestxt.length > 2)
+                                {
+                                    categoriestxt = categoriestxt.substring(0,categoriestxt.length - 2);
+                                }
+                                $('#alert_placeholder tbody').append('<tr><td>'+ this['title'] + ' </td> <td>'+categoriestxt+ '</td></tr>')
+
+                            });
+                        }
+
+                    },
+                    error: function(data) {
+                        retvalue == true
+                    }
+                });
+
+                if(retvalue == true)
+                {
+                    $(this).find(":button").attr('disabled', 'true');
+                    $(this).find(":submit").attr('disabled', 'true');
+                    $(this).find(":submit").val('Please wait..');
+                    //return true;
+
+                }
+                return retvalue;
+            });
+
         });
     </script>
 @endsection
