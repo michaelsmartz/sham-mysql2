49671248f769cec2619db968ddb6fc49e2523b9e
Unknown <taroon.gunessee@gmail.com>
2019-03-03 12:47:47 +0400

    Correction add new popup appear bug

--- resources/views/candidates/index.blade.php
+++ resources/views/candidates/index.blade.php
@@ -41,7 +41,7 @@
             @if(count($candidates) > 0)
             <div id="toolbar" class="shadow-eff1">
                 <div class="btn-group">
-                    <button id="item-create" type="button" class="btn btn-sham" data-wenk="Add new" data-wenk-pos="bottom" onclick="addForm(event)">
+                    <button id="item-create" type="button" class="btn btn-sham" data-wenk="Add new" data-wenk-pos="bottom">
                         <i class="glyphicon glyphicon-plus"></i> Add New
                     </button>
                 </div>
