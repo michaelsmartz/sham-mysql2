cd1ffb28447a3fbb839e0360a8f61cef0ee4e3f6
Unknown <taroon.gunessee@gmail.com>
2019-05-20 13:33:48 +0400

    Edit User changes to work on dashboard and portal

--- resources/views/partials/index.blade.php
+++ resources/views/partials/index.blade.php
@@ -19,36 +19,7 @@
         (function mainIIFE($) {
             "use strict";
 
-            var oldVal, $mainButton, loadUrl = function(url) {
-                $(".light-modal-heading").empty().html('');
-                $(".light-modal-footer .buttons").empty().html('');
-                $(".light-modal-body").empty().html('Loading...please wait...');
-                $.get(url).done(function(data) {
-                    $(".light-modal-heading").empty().html(data.title);
-                    $(".light-modal-body").empty().html(data.content);
-                    $(".light-modal-footer .buttons").empty().html(data.footer);
-                    $("#modalForm").attr('action',data.url);
-
-                    cleanUrlHash();
-
-                    $('.multipleSelect').each(function(){
-                        $(this).multiselect({
-                            submitAllLeft:false,
-                            sort: false,
-                            keepRenderingSort: false,
-                            search: {
-                                left: '<input type="text" name="q" class="form-control" placeholder="Search..." />',
-                                right: '<input type="text" name="q" class="form-control" placeholder="Search..." />',
-                            },
-                            fireSearch: function(value) {
-                                return value.length > 3;
-                            }
-                        });
-                    });
-                }).fail(function() {
-                    alerty.alert("An error has occurred. Please try again!",{okLabel:'Ok'});
-                });
-            };
+            var oldVal, $mainButton;
 
             $('#item-create,.item-create').click(function() {
                 var createUrl = $(this).data('create-url');
@@ -64,10 +35,12 @@
                 window.location = '{{url()->to("module_assessments")}}/' + id + '/responses';
             };
             
-            window.cleanUrlHash = function(){
-                window.history.pushState(null, "", window.location.href.replace("#light-modal", ""));
-                //history.replaceState(null, "", window.location.pathname);
-                //return window.location.hash.replace(/^#/, '');
+            window.pipelineSwitchState = function (id, event, candidate, newState) {
+                var route = '{{url()->current()}}/';
+
+                if (id) {
+                    loadUrl(route + id + '/switch/' + candidate + '/' + newState);
+                }
             };
 
             window.editEmployeeHistoryForm = function(id, event) {
@@ -76,23 +49,28 @@
                 }
             };
 
-            window.editForm = function(id, event, baseUrl) {
-                var route; 
+            window.addForm = function(event, baseUrl) {
+                var route;
                 if (baseUrl === void 0) {
-                    route = '{{url()->current()}}/';
+                    route = '<?php echo e(url()->current()); ?>/';
                 } else {
-                    route = '{{URL::to('/')}}/' + baseUrl + '/';
+                    route = '<?php echo e(URL::to('/')); ?>/' + baseUrl + '/';
                 }
 
-                if (id) {
-                    @if (isset($fullPageEdit) && $fullPageEdit == TRUE)
-                        window.location = route + id + '/edit';
-                    @else
-                        //$mainButton = $('.buttons button[type="submit"]');
-                        loadUrl(route + id + '/edit');
-                    @endif
-                }
+               loadUrl(route + 'create');
             };
+
+            window.addFormType = function(event,type_id,desc, baseUrl) {
+                var route;
+                if (baseUrl === void 0) {
+                    route = '<?php echo e(url()->current()); ?>/';
+                } else {
+                    route = '<?php echo e(URL::to('/')); ?>/' + baseUrl + '/';
+                }
+
+                loadUrl(route + 'create/'+ type_id +'/'+ desc);
+            };
+
 
             window.editFormAssessment = function(id, event, emp_id) {
                 var route;
