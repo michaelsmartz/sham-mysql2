e28d3e03ccf804b8b135d44ba07282b72af9230a
Unknown <taroon.gunessee@gmail.com>
2019-05-30 22:24:06 +0400

    FlushDashboardCachedQueries changes

--- app/Jobs/FlushDashboardCachedQueries.php
+++ app/Jobs/FlushDashboardCachedQueries.php
@@ -14,6 +14,8 @@ class FlushDashboardCachedQueries implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
+    public $summary;
+
     /**
      * Create a new job instance.
      *
@@ -21,7 +23,7 @@ class FlushDashboardCachedQueries implements ShouldQueue
      */
     public function __construct()
     {
-        //
+        $this->summary = [];
     }
 
     /**
@@ -33,15 +35,8 @@ class FlushDashboardCachedQueries implements ShouldQueue
     {
         try {
 
-            DB::table('job_logs')->insert([
-                'message' => 'About to run job',
-                'level' => 900,
-                'context' => 'FlushDashboardCachedQueries',
-                'extra' => '',
-                'created_at' => date('Y-m-d H:i:s'),
-            ]);
-
             Cache::tags('dashboard')->flush();
+            $this->summary = ['dashboard' => ['cache' => 'flushed']];
 
         } catch (Exception $exception) {
             
