0be892d68de97a189fecec32085d672ee3ce96da
Unknown <taroon.gunessee@gmail.com>
2019-06-04 11:54:33 +0400

    Job: UpdateUnclaimedMonthlyLeaves changes

--- app/Jobs/UpdateUnclaimedMonthlyLeaves.php
+++ app/Jobs/UpdateUnclaimedMonthlyLeaves.php
@@ -59,8 +59,11 @@ class UpdateUnclaimedMonthlyLeaves implements ShouldQueue
 
                 foreach($this->absenceTypes as $absenceType) {
                     $employees = $this->employeesQuery($absenceType)->where('employees.id', '=',4)->get();
-                    dump($employees[0]->eligibilities);
-                    die;
+                    if(! is_null($employees)){
+                        foreach($employees as $employee){
+                            $this->updateEligibilities($employee);
+                        }
+                    }
                 }
 
             }
@@ -79,9 +82,32 @@ class UpdateUnclaimedMonthlyLeaves implements ShouldQueue
       $ret =  Employee::employeesLite()->with(['eligibilities' => function ($query) use ( $absenceType) {
                 $query = $query->where('absence_type_id', '=', $absenceType->id)
                                ->where('start_date', '>=', $this->workYearStart)
-                               ->where('end_date', '<=', Carbon::now()->endOfMonth()->toDateString());
+                               ->where('end_date', '<=', Carbon::now()->endOfMonth()->toDateString())
+                               ->orderBy('start_date');
               }])->whereNull('date_terminated');
 
       return $ret;
+    }
+
+    private function updateEligibilities($employee)
+    {
+        $unclaimedTotal = 0;
+        if (sizeof($employee->eligibilities) > 0) {
+            // all items before last
+            $workCol = $employee->eligibilities->slice(0, -1);
+            // last item
+            $lastColItem = $employee->eligibilities->last();
+
+            foreach($workCol as $eligibility) {
+                //echo $eligibility->start_date, ' - ', $eligibility->end_date,' ',($eligibility->pivot->total - $eligibility->pivot->taken), '<br>';
+                $unclaimedTotal += ($eligibility->pivot->total - $eligibility->pivot->taken);
+            }
+            echo $unclaimedTotal, ' ',$lastColItem->pivot->total,'<br>';
+            if(!is_null($lastColItem)){
+                $lastColItem->pivot->total += $unclaimedTotal;
+                dump($lastColItem);
+                $lastColItem->pivot->save();
+            }
+        }
     }
 }
