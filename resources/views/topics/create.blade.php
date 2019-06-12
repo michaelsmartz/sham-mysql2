e3b31a9fe9a9b392391b287873e7bbe7deafaddd
Jason Polydor <jasonpolydor@gmail.com>
2019-06-12 14:32:29 +0400

    correction topic button hidden button save

--- resources/views/topics/create.blade.php
+++ resources/views/topics/create.blade.php
@@ -2,26 +2,24 @@
 @section('title', 'Add Topic')
 
 @section('content')
-    <div class="box box-primary">
-        <div class="box-body">
-            <form method="POST" action="{{ route('topics.store') }}" accept-charset="UTF-8" id="topic_form" name="topic_form" accept-charset="UTF-8" enctype="multipart/form-data">
-                {{ csrf_field() }}
-                <div class="row">
-                    <div class="col-sm-12">
-                        @include('topics.form', [
-                            'topic' => null,
-                            'uploader' => $uploader
-                        ])
-                    </div>
-                </div>
-                <div class="box-footer pull-right">
-                    <a href="{{ route('topics.index') }}" class="btn btn-default" title="Show all Topics">
-                        <span class="glyphicon glyphicon-th-list" aria-hidden="true"></span>
-                    </a>
-                    <button class="btn btn-primary" type="button" id="btnSave" data-loading-text="<i class='fa fa-spinner fa-spin'></i> Please wait">Save</button>
-
-                </div>
-            </form>
+    <form method="POST" action="{{ route('topics.store') }}" accept-charset="UTF-8" id="topic_form" name="topic_form" accept-charset="UTF-8" enctype="multipart/form-data">
+        {{ csrf_field() }}
+        <div class="row">
+            <div class="col-sm-10">
+                @include('topics.form', [
+                    'topic' => null,
+                    'uploader' => $uploader
+                ])
+            </div>
         </div>
-    </div>
+        <div class="row">
+            <div class="col-sm-9 text-right">
+                <a href="{{ route('topics.index') }}" class="btn btn-default" title="Show all Topics">
+                    <span class="glyphicon glyphicon-th-list" aria-hidden="true"></span>
+                </a>
+                <button class="btn btn-primary" type="button" id="btnSave" data-loading-text="<i class='fa fa-spinner fa-spin'></i> Please wait">Save</button>
+
+            </div>
+        </div>
+    </form>
 @endsection
