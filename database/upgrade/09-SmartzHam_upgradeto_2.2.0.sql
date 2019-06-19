8c3d557163c15eb2d8fbbfe36b1b522ae30b161c
Michael Veerapen <michaelv@smartz-solutions.com>
2019-06-19 20:26:30 +0400

    09-SmartzHam_upgradeto_2.2.0.sql : add calendar_events table

--- database/upgrade/09-SmartzHam_upgradeto_2.2.0.sql
+++ database/upgrade/09-SmartzHam_upgradeto_2.2.0.sql
@@ -10,3 +10,18 @@ CREATE TABLE `colours` (
 COLLATE='utf8mb4_0900_ai_ci'
 ENGINE=InnoDB
 ;
+
+CREATE TABLE `calendar_events` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `start_date` datetime NOT NULL,
+  `end_date` datetime NOT NULL,
+  `event_type` tinyint(4) NOT NULL,
+  `department_id` int(11) DEFAULT NULL,
+  `manager_only` int(11) DEFAULT NULL,
+  `calendable_id` int(11) NOT NULL,
+  `calendable_type` varchar(155) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
