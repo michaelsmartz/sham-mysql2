6cb56f09e632af3176849359e8ccd3951c8bd64f
Unknown <taroon.gunessee@gmail.com>
2019-06-19 12:51:38 +0400

    Colours table changes

--- database/upgrade/09-SmartzHam_upgradeto_2.2.0.sql
+++ database/upgrade/09-SmartzHam_upgradeto_2.2.0.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `colours` (
 	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`code` CHAR(6) NOT NULL DEFAULT '0' COMMENT 'Hex code without the #',
+	`code` CHAR(7) NOT NULL COMMENT 'Hex code with the #',
 	`created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	`updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	`deleted_at` DATETIME NULL DEFAULT NULL,
